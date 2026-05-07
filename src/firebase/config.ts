60d334c427cba1f08629c6218359d567c74f7358
Maa Jasol Boutique <maajasolboutique@gmail.com>
2026-05-07 08:56:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6440898737-e9e2e",
+  "appId": "1:904342295284:web:1c2b03ee16ef11317944a1",
+  "apiKey": "AIzaSyCEjvkse8_bUbuYm3xm5BfbjeIf75KgsvM",
+  "authDomain": "studio-6440898737-e9e2e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "904342295284"
+};
